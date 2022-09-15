087a394d61646d816424d314fcd7c44cff2342cf
Gabriel Lemos <jmjgb@outlook.com>
2022-09-15 01:20:21 -0300

    Imagens importadas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import './App.css';
+import sol from './images/sol.png'
+import mercurio from './images/mercurio.png'
+import venus from './images/venus.png'
+import terra from './images/terra.png'
+import marte from './images/marte.png'
+import jupiter from './images/jupiter.png'
+import saturno from './images/saturno.png'
+import urano from './images/urano.png'
+import netuno from './images/netuno.png'
 
 function App() {
   return (
